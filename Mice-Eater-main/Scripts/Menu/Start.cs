61c8e6bdff9ec093a26c263241d961c3e8c70afe
Superajm05 <antoniojames.muscio@gmail.com>
2025-06-12 00:14:43 +0200

    Creato una nuova versione del livello

--- Mice-Eater-main/Scripts/Menu/Start.cs
+++ Mice-Eater-main/Scripts/Menu/Start.cs
@@ -3,8 +3,10 @@ using System;
 
 public partial class Start : Button
 {
+	[Export] string path = "res://Scenes/level_v2_1.tscn";
+
 	public void StartGame()
 	{
-		GetTree().ChangeSceneToFile("res://Scenes/level_v1.tscn");
+		GetTree().ChangeSceneToFile(path);
 	}
 }
